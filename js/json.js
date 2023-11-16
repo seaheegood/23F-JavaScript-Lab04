3b7cfca4db1dc83ef8a193cc2b30db209d0c0974
Priyansh Thakar <priyanshthakar@gmail.com>
2023-11-16 18:43:39 -0500

    created json

--- js/json.js
+++ js/json.js
@@ -3,24 +3,24 @@ const header = document.querySelector("header");
 const section = document.querySelector("section");
 
 // STEP 3a: Create the asynchronous function populate()
+async function populate() {
+    // Introducing JavaScript Object Notation (JSON): https://json.org/
+    // STEP 4a: Create i-scream.json file with companyName, headOffice, established, active, topFlavors(name, calories, type, ingredients, image) */
+    // STEP 4b: Store the URL of a JSON file in a variable */
+    const url = "";
+    // STEP 5: Use the new URL to create a new request object
 
-// Introducing JavaScript Object Notation (JSON): https://json.org/
-// STEP 4a: Create i-scream.json file with companyName, headOffice, established, active, topFlavors(name, calories, type, ingredients, image) */
-// STEP 4b: Store the URL of a JSON file in a variable */
+    // STEP 6: Make a network request with the fetch() function, which returns a Response object
 
-// STEP 5: Use the new URL to create a new request object
+    // STEP 7: Capture the returned Response object and covert to a JSON object using json()
 
-// STEP 6: Make a network request with the fetch() function, which returns a Response object
+    // STEP 8: Output the iScream JSON object to the console 
 
-// STEP 7: Capture the returned Response object and covert to a JSON object using json()
+    // STEP 9a: Invoke the populateHeader function here, then build it below
 
-// STEP 8: Output the iScream JSON object to the console 
+    // STEP 10a: Invoke the showTopFlavors function here, then build it below
 
-// STEP 9a: Invoke the populateHeader function here, then build it below
-
-// STEP 10a: Invoke the showTopFlavors function here, then build it below
-
-
+}
 
 // STEP 3b: Call the populate() function
 
